fd1ead07c29bab2d5cdb8f238003e26331a4ae41
Felipe Silva <felipe.dacs@gmail.com>
2020-04-08 21:20:55 -0300

    release: add aula 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,49 @@ const express = require('express');
 
 const app = express();
 
-// localhost:3333/hello
-app.get('/hello', (request, response) => {
-    // request: informações da requisição
-    // response: informações para retornar ao cliente
-    return response.json({ message: 'Hello World '});
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informação
+ * POST: Criar informação
+ * PUT (toda)/PATCH (parcial): Alterar informação
+ * DELETE: Deletar informação
+ */
+
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
+});
+
+// mesmo endereço (recurso) método diferente
+// browser não consegue acessar com esse método, precisa de restclient (insomnia)
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+// localhost:3333/projects/1
+// browser não consegue acessar com esse método, precisa de restclient (insomnia)
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+// localhost:3333/projects/1
+// browser não consegue acessar com esse método, precisa de restclient (insomnia)
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
 });
 
 // escolha pessoal do Diego
